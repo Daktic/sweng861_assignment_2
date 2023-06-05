17c615e1c99e42c9120f9887569fe75a3b793ba7
Daktic <gberg@DakticDigital.com>
2023-06-04 20:54:51 -0400

    Now has 100% code coverage.

--- src/edu/psgv/sweng861/test/UnitsConvertorTest.java
+++ src/edu/psgv/sweng861/test/UnitsConvertorTest.java
@@ -39,16 +39,76 @@ class UnitsConvertorTest {
 
     @Test
     void main_ValidInput_Metric() {
-        // Arrange
-        String input = "10 cm\n";
-        String expectedOutput = "Please Enter the input value followed by the unit:\n" +
-                "10 cm is:\n" +
-                "3.9e+03 mil\n" +
-                "3.9 inch\n" +
-                "0.33 ft\n" +
-                "0.11 yard\n" +
-                "6.2e-05 mile\n"
+        // Testing Metric converts to Imperial
+
+        String inputMetric = "10 cm\n";
+        String expectedOutputMetric = """
+                Please Enter the input value followed by the unit:
+                10 cm is:
+                3.9e+03 mil
+                3.9 inch
+                0.33 ft
+                0.11 yard
+                6.2e-05 mile
+                \s"""
         ;
+
+        String normalizedExpectedOutputMetric = normalizeExpectedOutput(expectedOutputMetric);
+
+        InputStream sysInBackup = System.in;
+        PrintStream sysOutBackup = System.out;
+        try {
+            System.setIn(new ByteArrayInputStream(inputMetric.getBytes()));
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            UnitsConvertor.main(null);
+
+            assertEquals(normalizedExpectedOutputMetric, outContent.toString());
+        } finally {
+            System.setIn(sysInBackup);
+            System.setOut(sysOutBackup);
+        }
+
+        // Imperial
+        String inputImperial = "10 feet\n";
+        String expectedOutputImperial = """
+                Please Enter the input value followed by the unit:
+                10 feet is:
+                3048.000000 mm
+                304.800000 cm
+                3.048000 m
+                0.003048 km
+                \s"""
+                ;
+
+        String normalizedExpectedOutput = normalizeExpectedOutput(expectedOutputImperial);
+        
+        try {
+            System.setIn(new ByteArrayInputStream(inputImperial.getBytes()));
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            UnitsConvertor.main(null);
+
+            assertEquals(normalizedExpectedOutput, outContent.toString());
+        } finally {
+            System.setIn(sysInBackup);
+            System.setOut(sysOutBackup);
+        }
+    }
+
+    // Tests that the proper response is "Invalid input" if the code fails.
+    @Test
+    void main_InvalidInput_Metric() {
+
+        String input = "1 boot\n";
+        String expectedOutput = """
+                Please Enter the input value followed by the unit:
+                Invalid input
+                \s""";
+
+        String normalizedExpectedOutput = normalizeExpectedOutput(expectedOutput);
 
         InputStream sysInBackup = System.in;
         PrintStream sysOutBackup = System.out;
@@ -57,30 +117,48 @@ class UnitsConvertorTest {
             ByteArrayOutputStream outContent = new ByteArrayOutputStream();
             System.setOut(new PrintStream(outContent));
 
-            // Act
             UnitsConvertor.main(null);
 
-            // Assert
-            assertEquals(expectedOutput, outContent.toString());
+            assertEquals(normalizedExpectedOutput, outContent.toString());
+        } finally {
+            System.setIn(sysInBackup);
+            System.setOut(sysOutBackup);
+        }
+
+        String noInput = "\n";
+        String expectedOutputNoInput = """
+                Please Enter the input value followed by the unit:
+                Invalid input
+                \s""";
+
+        String normalizedExpectedOutputNoInput = normalizeExpectedOutput(expectedOutputNoInput);
+
+
+        try {
+            System.setIn(new ByteArrayInputStream(noInput.getBytes()));
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            UnitsConvertor.main(null);
+
+            assertEquals(normalizedExpectedOutputNoInput, outContent.toString());
         } finally {
             System.setIn(sysInBackup);
             System.setOut(sysOutBackup);
         }
     }
 
+    // Tests invalid input in the methods and makes sure it throws an exception.
+    @Test
+    void invalidInput() {
+        assertThrows(IllegalArgumentException.class, () -> UnitsConvertor.toMm(1, "test"), "Illegal unit value.");
+        assertThrows(IllegalArgumentException.class, () -> UnitsConvertor.toMil(1, "test"), "Illegal unit value.");
+    }
+
     //Tests if 1 meter unit is converted to mil
     @Test
     void toMil() {
         int numberInput = 1;
-        /*
-        1 m is:
-        39370.1 mil
-        39 inch
-        3.3 ft
-        1.1 yard
-        0.00062 mile
-         */
-        //TODO convert all outputs to normalized expected outputs
         assertEquals(39.3701, UnitsConvertor.toMil(numberInput ,"mm"));
         assertEquals(39.3701, UnitsConvertor.toMil(numberInput ,"millimeter"));
         assertEquals(393.701, UnitsConvertor.toMil(numberInput ,"cm"));
@@ -91,16 +169,10 @@ class UnitsConvertorTest {
         assertEquals(3.93701E7, UnitsConvertor.toMil(numberInput ,"kilometer"));
     }
 
+    // Tests 1 unit is converted to the proper number.
     @Test
     void toMm() {
         int numberInput = 1;
-        /*
-        1 ft is:
-        304.800000 mm
-        30.480000 cm
-        0.304800 m
-        0.000305 km
-         */
         assertEquals(0.0254, UnitsConvertor.toMm(numberInput ,"mil"));
         assertEquals(25.4, UnitsConvertor.toMm(numberInput ,"in"));
         assertEquals(25.4, UnitsConvertor.toMm(numberInput ,"inch"));
@@ -110,15 +182,5 @@ class UnitsConvertorTest {
         assertEquals(914.4, UnitsConvertor.toMm(numberInput ,"yard"));
         assertEquals(1609344.0, UnitsConvertor.toMm(numberInput ,"mi"));
         assertEquals(1609344.0, UnitsConvertor.toMm(numberInput ,"mile"));
-    }
-
-    @Test
-    void invalidInput() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            UnitsConvertor.toMm(1, "test");
-        }, "Illegal unit value.");
-        assertThrows(IllegalArgumentException.class, () -> {
-            UnitsConvertor.toMil(1, "test");
-        }, "Illegal unit value.");
     }
 }
